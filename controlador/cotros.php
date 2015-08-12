d9c009ee66cfb4d0ef5fc85b8c83b280e1e63b2b
Diego Felipe Monroy <difemo91@gmail.com>
2015-08-12 13:20:35 -0500

    Ingreso Informacion votros
    Ingreso del valor dependiendo de que boton se oprime

--- controlador/cotros.php
+++ controlador/cotros.php
@@ -7,25 +7,22 @@
 	$idreporteedit = isset($_POST['idreporte']) ? $_POST['idreporte'] : NULL;
 	$fecha       = isset($_POST['fecha']) ? $_POST['fecha'] : NULL;
 	$descripcion   = isset($_POST['descripcion']) ? $_POST['descripcion'] : NULL;
-	$salida        = isset($_POST['salida']) ? $_POST['salida'] : NULL;
-    $entrada       = isset($_POST['entrada']) ? $_POST['entrada'] : NULL;
-    $sal        = isset($_POST['sale']) ? $_POST['sale'] : NULL;
-    $ent       = isset($_POST['entra']) ? $_POST['entra'] : NULL;
+	$salida        = isset($_POST['valor']) ? $_POST['valor'] : NULL;
+    $entrada       = isset($_POST['valor']) ? $_POST['valor'] : NULL;
 	$idreporteeli  = isset($_POST['idreporteeli']) ? $_POST['idreporteeli'] : NULL;
 	$actu          = isset($_POST['actu']) ? $_POST['actu'] : NULL;
 	$idreporte     = isset($_GET['id']) ? $_GET['id'] : NULL;	
 
-    if ($sal) {
-        $salida = $_POST['valor'];
-        $entrada = "0";
-    }
-    if ($ent ) {
-        $salida = "0";
-        $entrada = $_POST['valor'];
-    }
-    
+    if (isset($_POST['Sale'])) {
+    //Out action
+    	$entrada=0;
+	}
+	if (isset($_POST['Entra'])) {
+    //In action
+		$salida=0;
+	}
 
-	if ($fecha && $descripcion && $salida && $entrada && !$actu) 
+	if ($fecha && $descripcion && !$actu) 
 	{
 		$otros->insertar_minutos($fecha, $descripcion, $salida, $entrada);
 	}
