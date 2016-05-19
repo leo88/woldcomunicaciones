3b4669be674c5d6ce7e583ae3d0c6d671604fe0f
DiFeMo <difemo91@gmail.com>
2016-05-19 09:07:55 -0500

    estilos x include

---
+++ vista/estilos.php
@@ -0,0 +1,7 @@
+<head>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css"/>
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.0.2/css/responsive.dataTables.min.css"/>
+  <link rel="stylesheet" href="css/prueba.css"/>
+  <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.0.2/js/dataTables.responsive.min.js"></script>
+</head>
--- vista/vcsst.php
+++ vista/vcsst.php
@@ -1,12 +1,5 @@
-
-<head>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css"/>
-  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.0.2/css/responsive.dataTables.min.css"/>
-  <link rel="stylesheet" href="css/prueba.css"/>
-  <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/responsive/2.0.2/js/dataTables.responsive.min.js"></script>
-</head>
 <?php 
+    include('estilos.php'); 
     include('modelo/mconsulta.php'); 
     $st = new Mconsulta();
 ?>
@@ -78,4 +71,4 @@
     </div>
 
     </div><!--/row-->
-<br/><br/> 
+<br/><br/>
