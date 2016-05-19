f464215b68b3bb484a3655d72e3080d38c09866d
leo88 <trianaleonardo1988@gmail.com>
2016-05-19 09:41:12 -0500

    archivos css y js

--- vista/estilos.php
+++ vista/estilos.php
@@ -1,7 +1,7 @@
 <head>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css"/>
-  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.0.2/css/responsive.dataTables.min.css"/>
+  <link rel="stylesheet" href="css/jquery.dataTables.min.css"/>
+  <link rel="stylesheet" href="hcss/responsive.dataTables.min.css"/>
   <link rel="stylesheet" href="css/prueba.css"/>
-  <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/responsive/2.0.2/js/dataTables.responsive.min.js"></script>
+  <script src="js/jquery.dataTables.min.js"></script>
+  <script src="js/dataTables.responsive.min.js"></script>
 </head>
